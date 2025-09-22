5f2b89bf6acf8dcaa375dbb12d0acf00f91271ad
Kanav Sharma <sharmakanavofficial05@gmail.com>
2025-09-23 00:25:28 +0530

    Real Balance

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,12 +1,31 @@
 import { Appbar } from "../components/Appbar"
 import { Users } from "../components/Users"
 import { Balance } from "../components/Balance"
+import { useEffect,useState } from "react"
+import axios from "axios"
 
 export const Dashboard=()=>{
+
+    const [balance,setBalance]=useState(null);
+
+    useEffect(()=>{
+        axios.get("http://localhost:3000/api/v1/account/balance",{
+            headers:{
+                Authorization:"Bearer "+localStorage.getItem("token")
+            },
+        })
+        .then((res)=>{
+            setBalance(res.data.balance);
+        })
+        .catch((e)=>{
+            console.error("Failed to fetch balance",err);
+        })
+    },[])
+
     return <div>
         <Appbar/>
         <div className="m-8">
-            <Balance value={""}/>
+            <Balance value={balance!==null ? balance : "Loading..."}/>
             <Users/>
         </div>
     </div>
